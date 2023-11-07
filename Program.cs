c54a5a5ac7a951c1399dc61ad006314aec6527eb
Maiko Duarte <mkdu185@gmail.com>
2023-11-07 18:03:16 -0300

    feat: Add desserializacao e atualizacao de dados

--- Program.cs
+++ Program.cs
@@ -2,38 +2,47 @@
 {
     static void Main()
     {
+        // Serialização da classe
+        var pessoa = new Pessoa { Nome = "Maiko", Idade = 31 };
+        string jsonPessoa = System.Text.Json.JsonSerializer.Serialize(pessoa);
 
-//serializacao da classe
-var pessoa = new Pessoa { Nome = "Maiko", Idade = 31};
-string jsonPessoa = System.Text.Json.JsonSerializer.Serialize(pessoa);
+        // Caminho do arquivo
+        string criarArquivoJson = "Pessoas.json";
 
+        // Cria o arquivo JSON
+        File.WriteAllText(criarArquivoJson, jsonPessoa);
 
-//caminho do arquivo
-string criarArquivoJson = "Pessoas.json";
+        Console.WriteLine("Dados da Pessoa serializados em Pessoas.json.");
 
-//cria o arquivo json
-File.WriteAllText(criarArquivoJson, jsonPessoa);
+        // Desserialização do JSON
+        if (File.Exists(criarArquivoJson))
+        {
+            string jsonTexto = File.ReadAllText(criarArquivoJson);
+            Pessoa pessoaDesserializada = System.Text.Json.JsonSerializer.Deserialize<Pessoa>(jsonTexto);
 
-// //desserializacao do json
-// Pessoa pessoaDesserializada = System.Text.Json.JsonSerializer.Deserialize<Pessoa>(criarArquivoJson);
+            if (pessoaDesserializada != null)
+            {
+                // Altera os dados
+                pessoaDesserializada.Idade = 32;
 
-// // altera os dados
-// pessoaDesserializada.Idade = 31;
+                // Serializa novamente os dados
+                string dadosAtualizados = System.Text.Json.JsonSerializer.Serialize(pessoaDesserializada);
 
-// // Serializa novamente os dados
-// string dadosAtualizados = System.Text.Json.JsonSerializer.Serialize(pessoaDesserializada);
+                // Caminho do arquivo JSON atualizado
+                string criarArquivoJsonAtualizado = "Pessoas_Dados_Atualizados.json";
 
-// //Caminho do arquivo json atualizado
-// string criarArquivoJsonAtualizado = "Pessoas_Dados_Atualizados.json";
-
-// //cria o novo arquivo json
-// File.WriteAllText(criarArquivoJsonAtualizado, dadosAtualizados);
-// Console.WriteLine("Os dados de Pessoa.json foram atualizados e estão no arquivo Pessoas_Dados_Atualizados.json");
-
-
+                // Cria o novo arquivo JSON atualizado
+                File.WriteAllText(criarArquivoJsonAtualizado, dadosAtualizados);
+                Console.WriteLine("Os dados de Pessoa.json foram atualizados e estão no arquivo Pessoas_Dados_Atualizados.json.");
+            }
+            else
+            {
+                Console.WriteLine("Falha na desserialização. O objeto é nulo.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"O arquivo {criarArquivoJson} não existe.");
+        }
     }
 }
-
-
-
-
