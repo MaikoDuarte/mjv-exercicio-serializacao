091c007ee0ef695235ceb509d3873adf02cdd5d6
Maiko Duarte <mkdu185@gmail.com>
2023-11-07 17:50:57 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+//serializacao da classe
+var pessoa = new Pessoa { Nome = "Maiko", Idade = 31};
+string jsonPessoa = System.Text.Json.JsonSerializer.Serialize(pessoa);
+
+
+//caminho do arquivo
+string criarArquivoJson = "Pessoas.json";
+
+//cria o arquivo json
+File.WriteAllText(criarArquivoJson, jsonPessoa);
+
+// //desserializacao do json
+// Pessoa pessoaDesserializada = System.Text.Json.JsonSerializer.Deserialize<Pessoa>(criarArquivoJson);
+
+// // altera os dados
+// pessoaDesserializada.Idade = 31;
+
+// // Serializa novamente os dados
+// string dadosAtualizados = System.Text.Json.JsonSerializer.Serialize(pessoaDesserializada);
+
+// //Caminho do arquivo json atualizado
+// string criarArquivoJsonAtualizado = "Pessoas_Dados_Atualizados.json";
+
+// //cria o novo arquivo json
+// File.WriteAllText(criarArquivoJsonAtualizado, dadosAtualizados);
+// Console.WriteLine("Os dados de Pessoa.json foram atualizados e estão no arquivo Pessoas_Dados_Atualizados.json");
+
+
+    }
+}
+
+
+
+
